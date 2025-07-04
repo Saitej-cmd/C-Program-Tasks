3436fa2923b1730cc2a85b5ed2255194ae720f59
Saitej-cmd <saitejpalli@gmail.com>
2025-07-04 09:48:44 +0530

    Update task1.c

--- task1.c
+++ task1.c
@@ -4,8 +4,6 @@
 int main() {
     FILE *fp;
     char data[100];
-
-    // 1. Create and Write to a file
     fp = fopen("myfile.txt", "w");  // "w" mode creates a file if it doesn't exist
     if (fp == NULL) {
         printf("Error creating file.\n");
@@ -16,8 +14,6 @@ int main() {
     fputs(data, fp);
     fclose(fp);
     printf("Data written successfully.\n");
-
-    // 2. Append data to the file
     fp = fopen("myfile.txt", "a");  // "a" mode opens file for appending
     if (fp == NULL) {
         printf("Error opening file for appending.\n");
@@ -28,8 +24,6 @@ int main() {
     fputs(data, fp);
     fclose(fp);
     printf("Data appended successfully.\n");
-
-    // 3. Read data from the file
     fp = fopen("myfile.txt", "r");  // "r" mode opens file for reading
     if (fp == NULL) {
         printf("Error opening file for reading.\n");
